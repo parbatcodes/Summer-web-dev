d77af7d8042cf4afb3cfe3bbcee1695f9c38b967
Parbat Singh <99901618+parbatcodes@users.noreply.github.com>
2023-07-17 21:13:25 +0530

    Add files via upload

---
+++ Simon-Game/mylogic.js
@@ -0,0 +1,46 @@
+let i = 0;
+let sequence = [];
+
+function seq() {
+    for(i=0;i<10;i++){
+        sequence[i]= Math.floor(Math.random()*4)+1;
+    }
+}
+
+$(document).keydown(()=>{
+    seq();
+    for(i=0;i<10;i++){
+        alert(i);
+        let press = sequence[i];
+        switch (press) {
+            case 1:
+                $(".green").addClass("pressed");
+                setTimeout(() => {
+                   $(".green").removeClass("pressed"); 
+                }, 100);
+                break;
+            
+            case 2:
+                $(".red").addClass("pressed");
+                setTimeout(() => {
+                    $(".red").removeClass("pressed"); 
+                 }, 100);
+                break;
+            case 3:
+                $(".yellow").addClass("pressed");
+                setTimeout(() => {
+                    $(".yellow").removeClass("pressed"); 
+                 }, 100);
+                break;
+            case 4:
+                $(".blue").addClass("pressed")
+                break;
+            default:
+                continue;
+                break;
+        }
+    }
+
+
+})
+
